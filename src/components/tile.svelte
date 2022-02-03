ea2d13137e468d2c2d107572b71f4f8234ff0b6a
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2022-02-02 19:01:59 -0600

    Fix mobile compatiblity

--- src/components/tile.svelte
+++ src/components/tile.svelte
@@ -18,12 +18,12 @@
 <style>
     .tile {
         width: 100px;
-        height: 100px;
         border: 1px solid black;
         display: flex;
         justify-content: center;
         align-items: center;
         border: 4px solid red;
+        position: relative;
     }
     .incorrect {
         background-color: #3a3a3c;
@@ -48,7 +48,7 @@
         border: 4px solid #FF0000;
     }
     .letter {
-        font-size: 4em;
+        font-size: min(10vw, 4em);
         font-family: "Roboto", sans-serif;
         color: white;
         text-transform: uppercase;
